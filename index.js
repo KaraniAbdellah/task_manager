082d163beaa50ba586b9390a36c2f1b00242aaf0
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-23 17:56:50 +0100

    make request to DB

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// start here!!!
+
+
+const mongoose = require("./pack-mongoose/node_modules/mongoose");
+
+// Connect to Mongo DB
+mongoose.connect("mongodb://localhosts:27017/test").then(()=> {
+    console.log("MongoDb Connected");
+}).catch((err) => {
+    console;log("error", err);
+});
+
+
+// Define The Rules of the data that will send to the DataBase
+const tutShema = new mongoose.Schema({
+    name:  {
+        type: String,
+        required: true
+    }
+});
+
+
+// Create a Collection (all document will in collection)
+const collection = new mongoose.model("myModel", tutShema);
+
+
+
+
+
