9d5cb6f10457f248130e91688e5aee57d913c81c
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-27 16:00:08 +0100

    remove test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-async');
-
-async function getUserOperation() {
-    await prompt.start(); // Start the prompt
-    const { operation } = await prompt.get(['operation']); // Prompt user for input
-    console.log(`You selected: ${operation}`);
-}
-
-getUserOperation();
